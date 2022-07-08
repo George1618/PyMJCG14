c7bbc125573c95b6dfd3a1944b5708f4d38bf9eb
Bruno Sousa <bbarros.sousa@hotmail.com>
2022-07-08 10:48:49 -0300

    Update util.py

--- pymjc/util.py
+++ pymjc/util.py
@@ -16,11 +16,11 @@ class Converter():
     def to_SEQ(list: List[tree.Stm])-> tree.Stm:
         seq = tree.SEQ(list[0], list[1])
         for i in range(2, len(list)):
-            seq = tree.SEQ(list.get(i), seq)
+            seq = tree.SEQ(list[i], seq)
         return seq
 
-    def to_ExpList(list: List[tree.Exp])-> tree.ExpList:
+    def to_ExpList(list: [tree.Exp])-> tree.ExpList:
         exp_list: tree.ExpList = None
         for i in range(len(list)):
-            exp_list = tree.ExpList(list.get(i), exp_list)
+            exp_list = tree.ExpList(list[i], exp_list)
         return exp_list
